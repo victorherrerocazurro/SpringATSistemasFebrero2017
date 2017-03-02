58d1a765cb8be30e3b90b3c6fe6cb611da7bf4ec
ATSTAFF <ATSTAFF>
2017-03-02 17:18:06 +0100

    Modificaciones en Hibernate

--- 13_ORM/src/main/java/com/atsistemas/curso/entidades/Factura.java
+++ 13_ORM/src/main/java/com/atsistemas/curso/entidades/Factura.java
@@ -14,8 +14,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import org.hibernate.type.CalendarType;
-
 @Entity
 @Table(name="FACTURAS")
 //@Access(AccessType.PROPERTY)
--- 13_ORM/src/main/java/com/atsistemas/curso/persistencia/hibernate/HibernateFacturaDao.java
+++ 13_ORM/src/main/java/com/atsistemas/curso/persistencia/hibernate/HibernateFacturaDao.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
